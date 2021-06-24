b3758efe8d0c0ce264f498c3972ae4a46094fad3
OrangeSpork <57274555+OrangeSpork@users.noreply.github.com>
2021-06-24 12:42:47 -0600

    Version Update

--- AdvIKPlugin/AdvIKPlugin.cs
+++ AdvIKPlugin/AdvIKPlugin.cs
@@ -26,7 +26,7 @@ namespace AdvIKPlugin
     {
         public const string GUID = "orange.spork.advikplugin";
         public const string PluginName = "AdvIKPlugin";
-        public const string Version = "1.6.1";
+        public const string Version = "1.6.2";
 
         public static AdvIKPlugin Instance { get; set; }
 
